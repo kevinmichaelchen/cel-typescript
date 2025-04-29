d9ea4781fcc27034547e205530b36aeedac23742
Kevin Chen <kevinmichaelchen@gmail.com>
2025-04-29 16:22:10 -0400

    chore: fix moves

--- libs/core/scripts/build.sh
+++ libs/core/scripts/build.sh
@@ -9,13 +9,19 @@ mkdir -p artifacts
 
 if [ "$USE_ZIG" = "true" ]; then
   npx @napi-rs/cli build --platform --target "$1" --release --zig \
-    --js artifacts/native.cjs --dts artifacts/native.d.ts
+    --js artifacts/native.cjs \
+    --dts artifacts/native.d.ts \
+    --js-package-name @kevinmichaelchen/cel-typescript
 else
   npx @napi-rs/cli build --platform --target "$1" --release \
-    --js artifacts/native.cjs --dts artifacts/native.d.ts
+    --js artifacts/native.cjs \
+    --dts artifacts/native.d.ts \
+    --js-package-name @kevinmichaelchen/cel-typescript
 fi
 
-cp @kevinmichaelchen/*.node artifacts
+# TODO we only want to do this locally so unit tests pass
+cp *.node artifacts
 
+# TODO we do not want to put binaries into dist
 mkdir -p dist/artifacts
 cp -r artifacts/ dist/artifacts
