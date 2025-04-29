83ea1a295aef562ac76c36b5673852da900e0517
Kevin Chen <kevinmichaelchen@gmail.com>
2025-04-29 12:48:37 -0400

    chore: new loader file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
+# See https://napi.rs/docs/cli/build for details
+
 if [ "$USE_ZIG" = "true" ]; then
-  npx @napi-rs/cli build --platform --target "$1" --js src/native.cjs --release --zig
+  npx @napi-rs/cli build \
+    # Add platform triple to the .node file (e.g., [name].linux-x64-gnu.node)
+    --platform \
+    
+    # Target triple (e.g., x86_64-apple-darwin)
+    --target "$1" \
+    
+    # The filename and path of the JavaScript binding file
+    --js src/native.cjs \
+    
+    # TypeScript declaration file
+    --dts src/native.d.ts \
+    
+    # Bypass to cargo build --release
+    --release \
+
+    # @napi-rs/cli will use zig as cc / cxx and linker to build your program.
+    --zig
+
 else
-  npx @napi-rs/cli build --platform --target "$1" --js src/native.cjs --release
+  npx @napi-rs/cli build \
+    --platform \
+    --target "$1" \
+    --js src/native.cjs \
+    --dts src/native.d.ts \
+    --release
+
 fi
